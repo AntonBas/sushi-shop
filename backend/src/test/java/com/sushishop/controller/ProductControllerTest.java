be89af5a3224bbd1694de166fe281aaf99fe593f
AntonBas <basantonoleg@gmail.com>
2026-06-26 23:10:16 +0200

    feat: update tests

--- backend/src/test/java/com/sushishop/controller/ProductControllerTest.java
+++ backend/src/test/java/com/sushishop/controller/ProductControllerTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ProductControllerTest {
+public class ProductControllerTest {
 
     private MockMvc mockMvc;
 
--- backend/src/test/java/com/sushishop/controller/UserControllerTest.java
+++ backend/src/test/java/com/sushishop/controller/UserControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("test")
-class UserControllerTest {
+public class UserControllerTest {
 
     private MockMvc mockMvc;
 
