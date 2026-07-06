35479ed8149895b93897009ed923afab36c12a62
AntonBas <basantonoleg@gmail.com>
2026-07-06 19:32:52 +0200

    feat: configure frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { NotificationProvider } from './context/NotificationContext'
 import NotificationContainer from './components/ui/Notification/NotificationContainer'
 import { AuthProvider } from './context/AuthContext'
@@ -8,11 +9,13 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NotificationProvider>
-      <AuthProvider>
-        <App />
-        <NotificationContainer />
-      </AuthProvider>
-    </NotificationProvider>
+    <BrowserRouter>
+      <NotificationProvider>
+        <AuthProvider>
+          <App />
+          <NotificationContainer />
+        </AuthProvider>
+      </NotificationProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
