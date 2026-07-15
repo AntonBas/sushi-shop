6eda9c8571f1980031e4b3779c5d552d2ad52581
AntonBas <basantonoleg@gmail.com>
2026-07-15 22:48:22 +0200

    feat: add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import MainLayout from './layout/MainLayout/MainLayout'
+import Home from './pages/Home/Home'
+import Login from './pages/auth/Login/Login'
+import Register from './pages/auth/Register/Register'
+import EmailVerification from './pages/auth/EmailVerification/EmailVerification'
+import OAuth2Redirect from './pages/auth/OAuth2Redirect/OAuth2Redirect'
+import ForgotPassword from './pages/auth/ForgotPassword/ForgotPassword'
+import ResetPassword from './pages/auth/ResetPassword/ResetPassword'
 
 function App() {
   return (
     <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Route>
+      <Route path="/verify-email" element={<EmailVerification />} />
+      <Route path="/oauth2/redirect" element={<OAuth2Redirect />} />
+
     </Routes>
   )
 }
