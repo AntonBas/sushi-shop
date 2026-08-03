dc7bbb4db083c4fa77264dd7f7bd953cb68895f4
AntonBas <basantonoleg@gmail.com>
2026-08-03 22:17:45 +0200

    feat: remove unused method

--- backend/src/main/java/com/sushishop/promotion/PromotionRepository.java
+++ backend/src/main/java/com/sushishop/promotion/PromotionRepository.java
@@ -6,7 +6,5 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-    List<Promotion> findByActiveTrueAndEndDateAfter(LocalDateTime now);
-
     List<Promotion> findByStartDateBeforeAndEndDateAfter(LocalDateTime start, LocalDateTime end);
 }
