f4f913efa0543b68f8ecb7d06d7ba410480f1ee4
AntonBas <basantonoleg@gmail.com>
2026-08-04 21:56:03 +0200

    feat: redesign home page with hero section

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,14 @@
-import PromotionsSection from '../../components/Home/PromotionsSection/PromotionsSection'
-import PopularSection from '../../components/Home/PopularSection/PopularSection'
-import MenuSection from '../../components/Menu/MenuSection'
-import styles from './Home.module.css'
+import HeroSection from "../../components/Home/HeroSection/HeroSection";
+import PopularSection from "../../components/Home/PopularSection/PopularSection";
+import PromotionsSection from "../../components/Home/PromotionsSection/PromotionsSection";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <MenuSection />
+      <HeroSection />
       <PopularSection />
       <PromotionsSection />
     </div>
-  )
+  );
 }
